0547585639ea2e043a8b8cd780e1c19c8ed56057
Matthew Dolan <info@mattdolan.com>
2017-10-21 15:55:41 +0100

    Adding testutils project to better share common test code

--- layercache-ehcache/src/test/kotlin/com/appmattus/layercache/TestException.kt
+++
@@ -1,3 +0,0 @@
-package com.appmattus.layercache
-
-class TestException : Exception()
--- layercache-serializer/src/test/kotlin/com/appmattus/layercache/TestUtils.kt
+++
@@ -1,8 +0,0 @@
-package com.appmattus.layercache
-
-class TestUtils {
-    companion object {
-        @Suppress("UNCHECKED_CAST")
-        fun <T> uninitialized(): T = null as T
-    }
-}
