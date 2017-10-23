682d96b14b092fce97db6f5cba0e15b0d30cbe35
Matthew Dolan <info@mattdolan.com>
2017-10-23 21:53:08 +0100

    Removing connectedCheck tests
    Issues with running on travis so removing.

--- scripts/wait-for-emulator19.sh
+++
@@ -1,7 +0,0 @@
-IT_CMD="adb -s emulator-5556 wait-for-device shell getprop init.svc.bootanim"
-
-until $WAIT_CMD | grep -m 1 stopped; do
-  echo "Waiting for emulator19..."
-  sleep 1
-done
-
--- scripts/wait-for-emulator23.sh
+++
@@ -1,7 +0,0 @@
-IT_CMD="adb -s emulator-5558 wait-for-device shell getprop init.svc.bootanim"
-
-until $WAIT_CMD | grep -m 1 stopped; do
-  echo "Waiting for emulator23..."
-  sleep 1
-done
-
