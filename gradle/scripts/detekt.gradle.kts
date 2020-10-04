c823a6802607f3207cb14efba5494c622e4293bb
Matthew Dolan <matt.dolan@babylonhealth.com>
2020-10-04 14:28:11 +0100

    Update to latest dependencies

--- gradle/scripts/detekt.gradle.kts
+++ gradle/scripts/detekt.gradle.kts
@@ -22,7 +22,7 @@ buildscript {
         maven(url = "https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.13.1")
+        classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.14.1")
     }
 }
 
@@ -58,5 +58,5 @@ tasks.named("detekt", Detekt::class.java).configure {
 }
 
 dependencies {
-    "detektPlugins"("io.gitlab.arturbosch.detekt:detekt-formatting:1.13.1")
+    "detektPlugins"("io.gitlab.arturbosch.detekt:detekt-formatting:1.14.1")
 }
